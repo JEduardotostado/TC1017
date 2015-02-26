b11539d1dee39c058ae89f7f7371859c71d9458a
JEduardotostado <j_aquitoy@hotmail.com>
2015-02-26 09:30:47 -0600

    WSQ03
    Here´s my code for WSQ03

---
+++ WSQ03.cpp
@@ -0,0 +1,20 @@
+// Fun with numbers 
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int x;
+int y;
+cout <<"Please enter a numerical value:";
+cin >> x;
+cout << "Please enter another numerical value:";
+cin >> y;
+cout <<"The sum of the two numbers is: " <<x+y << endl;
+cout << "The difference of the two numbers is: " <<x-y << endl;
+cout << "The product of the two numbers is: " <<x*y << endl;
+cout << "The result of the division of the two numbers is: " <<x/y << endl;
+cout << "The remainder of the integer division of the two numbers is: " <<x%y;
+
+	return 0;
+}
